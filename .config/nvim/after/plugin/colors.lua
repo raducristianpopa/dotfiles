eb0987a38823cbc8ee059a28e5ab86e288f3ca45
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-06-10 00:36:09 +0300

    Update font

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,12 +1,17 @@
-local status, nightfox = pcall(require, "nightfox")
+local status, ncf = pcall(require, "no-clown-fiesta")
 if (not status) then return end
 
 -- Default options
-nightfox.setup({
-    groups = {
-        all = {
-            EndOfBuffer = { link = "Conceal" },
-        }
+ncf.setup({
+    transparent = false, -- Enable this to disable the bg color
+    styles = {
+        -- You can set any of the style values specified for `:h nvim_set_hl`
+        comments = {},
+        keywords = {},
+        functions = {},
+        variables = {},
+        type = {},
+        lsp = {},
     }
 })
 
