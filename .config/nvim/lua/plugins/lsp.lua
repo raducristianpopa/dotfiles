c9780f6f18f31f84c3d3984b443ebf156a9c6bfd
Radu-Cristian Popa <praducristian@gmail.com>
2024-08-11 10:04:38 +0300

    Fix prettier timeout and update font

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -221,7 +221,6 @@ return {
             notify_on_error = false,
             default_format_opts = {
                 async = true,
-                timeout_ms = 500,
                 lsp_format = "fallback",
             },
             format_after_save = nil,
